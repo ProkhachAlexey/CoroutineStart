25a6af5887e74fe7e4376738757213631f47aae4
Alexey Prokhach <asprokhach68@gmail.com>
2022-09-29 22:04:19 +0300

    Initial commit

---
+++ app/src/main/java/com/prokhach/coroutinestart/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.prokhach.coroutinestart
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.prokhach.coroutinestart.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            loadData()
+        }
+    }
+//
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        loadCity {
+            binding.tvLocation.text = it
+            loadTemperature(it) {
+                binding.tvTemperature.text = it.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback.invoke("Moscow")
+        }
+
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        Toast.makeText(
+            this,
+            city,
+            Toast.LENGTH_SHORT
+        ).show()
+        Thread.sleep(5000)
+        callback.invoke(17)
+    }
+}
